9d451d4ef8e476553ed8e92f37814796b1e2b05d
Caio Henrique <caioadriana5@gmail.com>
2021-06-28 21:13:53 -0300

    amanhaeterca

--- InstaDev/Controllers/PostController.cs
+++ InstaDev/Controllers/PostController.cs
@@ -1,7 +1,64 @@
+using System.IO;
+using InstaDev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InstaDev.Controllers
 {
-    public class PostController
+    [Route("Postagem")]
+    public class PostController : Controller
     {
+        Post postModel = new Post();
+
+        [Route("listar")]
+        public IActionResult Index()
+        {
+
+            ViewBag.Post = postModel.LerTodas();
+            return View();
+        }
+
         
+        [Route("CriarPost")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Post novaPostagem = new Post();
+            novaPostagem.Descrição = form["Descrição"];
+
+            if (form.Files.Count > 0)
+            {
+
+                var file = form.Files[0];
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            else{
+                novaPostagem.ImagemPost = "padrao.png";
+            }
+
+            postModel.Criar(novaPostagem);
+
+            ViewBag.Post = postModel.LerTodas();
+
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+        [Route("{id}")]
+        public IActionResult Excluir(string id){
+            postModel.Deletar(id);
+            ViewBag.Post = postModel.LerTodas();
+            return LocalRedirect("~/Equipe/Listar");
+        }
     }
 }
