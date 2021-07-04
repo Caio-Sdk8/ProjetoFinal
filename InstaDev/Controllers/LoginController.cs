58c3bafdefc7912e8c0935f5373b726faec7a818
Miguel Candido <atendimento@server.eventus.com.br>
2021-07-04 15:06:18 -0300

    Instadev com login 0.1

--- InstaDev/Controllers/LoginController.cs
+++ InstaDev/Controllers/LoginController.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Collections.Generic;
+using InstaDev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InstaDev.Controllers
 {
-    public class LoginController
+     [Route("Login")]
+    public class LoginController : Controller
     {
+         [TempData]
+        public string mensagem { get; set; }
         
+        
+        Usuario usermodel = new Usuario();
+
+        public IActionResult Index(){
+            return View();
+        }
+        [Route("Logar")]
+        public IActionResult Logar(IFormCollection form){
+            List<string> userCSV = usermodel.lertodaslinhasCSV("Database/jogador.csv");
+            var logado = userCSV.Find(x => x.Split(";")[3] == form["Email"] && x.Split(";")[4] == form["Senha"]);
+            var tentativa = form["Email"];
+            
+            if (logado != null)
+            {
+                HttpContext.Session.SetString("Username", logado.Split(";")[1]);
+                return LocalRedirect("~/");
+            }
+            return LocalRedirect("~/Login");
+            
+        }
+
+        [Route("Logout")]
+        public IActionResult logout(){
+            HttpContext.Session.Remove("Username");
+            return LocalRedirect("~/");
+        }
     }
 }
